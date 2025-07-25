92dfe2d3cb1aa0c9355c92971f84944a14f5a0c2
Fazal <meetfazalkarim@gmail.com>
2025-07-25 10:59:54 +0500

    TESTED

--- index.js
+++ index.js
@@ -7,9 +7,14 @@ const PORT = process.env.PORT || 5050;
 app.use(cors());
 app.use(express.json());    
 
-app.get('/api/hello', (req, res) => {
+app.get('/api/hello-world', (req, res) => {
   res.json({ message: 'Hello World!' });
 });
+
+app.get('/api/hello-universe', (req, res) => {
+  res.json({ message: 'Hello Universe!' });
+});
+
 
 app.listen(PORT, () => {
   console.log(`🚀 Server running on http://localhost:${PORT}`);
