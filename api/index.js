f2bd0c36773b08d45f439e0e4c7d4a65781b5702
Fazal <meetfazalkarim@gmail.com>
2025-07-25 16:53:05 +0500

    TESTED

--- api/index.js
+++ api/index.js
@@ -1,18 +1,22 @@
-const express = require('express');
-const cors = require('cors');
-const serverless = require('serverless-http');
+// const express = require('express');
+// const cors = require('cors');
+// const serverless = require('serverless-http');
 
-const app = express();
+// const app = express();
 
-app.use(cors());
-app.use(express.json());
+// app.use(cors());
+// app.use(express.json());
 
-app.get('/hello-world', (req, res) => {
-  res.json({ message: 'Hello World!' });
-});
+// app.get('/hello-world', (req, res) => {
+//   res.json({ message: 'Hello World!' });
+// });
 
-app.get('/hello-universe', (req, res) => {
-  res.json({ message: 'Hello Universe!' });
-});
+// app.get('/hello-universe', (req, res) => {
+//   res.json({ message: 'Hello Universe!' });
+// });
 
-module.exports = serverless(app);
+// module.exports = serverless(app);
+
+export default function handler(req, res) {
+  res.status(200).json({ message: "Hello from Vercel Node.js API!" });
+}
